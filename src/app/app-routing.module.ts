95493d39d3d202d6e4d952777a2804700e96b26f
Mariia Moroz <mashanya.kov@gmail.com>
2023-06-01 16:23:47 +0200

    added all routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from "@angular/core";
 import { Route, RouterModule } from "@angular/router";
+import { AboutComponent } from "./about /about.component";
 import { CalculatorComponent } from "./calculator/calculator.component";
+import { IngredientsComponent } from "./ingredients/ingredients.component";
 
 const routes: Route[] = [
- { path: 'calculator' , component: CalculatorComponent }
+  { path: 'calculator' , component: CalculatorComponent },
+  { path: 'ingredients' , component: IngredientsComponent },
+  { path: 'about' , component: AboutComponent },
+  { path: '' , redirectTo: 'calculator', pathMatch: 'full' },
 ]
 
 
