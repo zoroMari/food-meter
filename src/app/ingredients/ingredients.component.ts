0a1d43ca0331c34f88161385fbb516c79276a689
Mariia Moroz <98117895+zoroMari@users.noreply.github.com>
2023-06-02 18:27:55 +0200

    Ingredients (#2)
    created table of ingredients

--- src/app/ingredients/ingredients.component.ts
+++ src/app/ingredients/ingredients.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IIngredient } from 'src/ingredients.model';
+import { IngredientsService } from './ingredients.service';
 
 @Component({
   selector: 'app-ingredients',
@@ -6,10 +8,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ingredients.component.sass']
 })
 export class IngredientsComponent implements OnInit {
+  public displayedColumns: string[] = ['name', 'gram', 'ccal', 'protein', 'carbon', 'fat', 'edit', 'delete', 'save', 'cancel'];
+  public dataSource!: IIngredient[];
 
-  constructor() { }
+
+  public isEditMode!: boolean;
+
+  constructor(
+    private _ingrService: IngredientsService,
+  ) { }
 
   ngOnInit(): void {
+    this._ingrService.isEditMode.subscribe(
+      (value) => this.isEditMode = value
+    )
+
+    this._ingrService.ingredientsChange.subscribe(
+      (value: IIngredient[]) => this.dataSource = value
+    )
+  }
+
+  handleEditIngredient(ingr: IIngredient) {
+    this._ingrService.editIngredient(ingr);
+  }
+
+  handleSaveChanges(ingr: IIngredient) {
+    this._ingrService.saveChangedIngredient(ingr);
+  }
+
+  handleCancelChanges() {
+    this._ingrService.cancelChangedIngredient();
+  }
+
+  handleDeleteIngredient(ingr: IIngredient) {
+    this._ingrService.deleteIngredient(ingr);
   }
 
 }
--- src/app/ingredients/ingredients.service.ts
+++ src/app/ingredients/ingredients.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
 import { IIngredient } from "src/ingredients.model";
 
 @Injectable({
@@ -9,14 +10,40 @@ export class IngredientsService {
     { name: 'Egg', gram: 100, ccal: 5, protein: 10, carbon: 15, fat: 20 },
     { name: 'Chicken',  gram: 100, ccal: 12, protein: 20, carbon: 30, fat: 40 },
     { name: 'Salmon', gram: 100,  ccal: 14, protein: 20, carbon: 32, fat: 50 },
-    // { name: 'Bread',  gram: 100, ccal: 8, protein: 14, carbon: 30, fat: 60 },
+    { name: 'Bread',  gram: 100, ccal: 8, protein: 14, carbon: 30, fat: 60 },
   ];
 
+  public ingredientsChange = new BehaviorSubject<IIngredient[]>(this.ingredients);
+  public isEditMode = new BehaviorSubject<boolean>(false);
+
   public get ingredients() {
-    return this._ingredients
+    return JSON.parse(JSON.stringify(this._ingredients))
   }
 
+  public editIngredient(ingr: IIngredient) {
+    this.isEditMode.next(true);
+  }
 
+  public saveChangedIngredient(newIngr: IIngredient) {
+    this.isEditMode.next(false);
+    const ind = this._ingredients.findIndex((item) => item.name === newIngr.name);
 
+    if (ind === -1) return;
+    else {
+      this._ingredients[ind] = newIngr;
+      this.ingredientsChange.next(this.ingredients);
+    }
+  }
 
+  public cancelChangedIngredient() {
+    this.isEditMode.next(false);
+    this.ingredientsChange.next(this.ingredients);
+  }
+
+  public deleteIngredient(ingr: IIngredient) {
+    const ind = this._ingredients.findIndex((item) => item.name === ingr.name);
+
+    this._ingredients.splice(ind, 1);
+    this.ingredientsChange.next(this.ingredients);
+  }
 }
